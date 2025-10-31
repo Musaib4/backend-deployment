92f12d1a08558c3e0942d5bd37ee05c6bb8764e0
Musaib Misger <musaibmisger0@gmail.com>
2025-10-31 20:34:14 +0530

    second backend commit

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
-const port = 4000
+// use either the .env port (for local) OR Render's port
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/secPage', (req, res) => {
-  res.send('Hello we!')
-})
+  res.send('Hello we!');
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
