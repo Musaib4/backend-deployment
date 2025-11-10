60c09cf58d384f44a026ef45408c1942d3e52597
Musaib Misger <musaibmisger0@gmail.com>
2025-11-10 22:42:04 +0530

    first approach to connect DB

---
+++ c backProj/src/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants"
+
+import express from "express"
+
+
+// first approach to connect the database
+
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGO_DBURI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("err:",error)
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is Listening on Port${process.env.PORT}`)
+    })
+
+    } catch(error) {
+        console.log("error: ", error)
+        throw err
+    }
+})()
