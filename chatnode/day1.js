8f94a8069089bd039c1953f309ed0b8703608c02
Musaib Misger <musaibmisger0@gmail.com>
2025-11-20 22:44:15 +0530

    nodejs revision day1

---
+++ chatnode/day1.js
@@ -0,0 +1,7 @@
+console.log("hello node")
+
+console.log("Directory:", process.cwd());
+console.log("Node Version:", process.version);
+console.log("Platform:", process.platform);
+
+console.log(__filename);
